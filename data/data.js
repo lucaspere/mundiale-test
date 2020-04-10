64d767b7593bc49485ccd574f6ed90667a4b1b21
lucaspere <lucasrupestre@hotmail.com>
2020-04-10 11:45:39 -0300

    adicionando o employee card

---
+++ data/data.js
@@ -0,0 +1,29 @@
+
+import eduardo from '../public/images/eduardo.jpg'
+import daniel from '../public/images/daniel.jpg'
+export const data = [
+   {
+      id: '1',
+      employeeName: "Eduardo Viana",
+      role: "UX Designer",
+      avatar: eduardo ,
+      teleNumbers: [
+         '31|9.9147-8701',
+         '31|2138-2020'
+      ],
+      email: 'eduardo.viana@mundiale.com.br',
+      qrCode: require('../public/images/qrCode.png')
+   },
+   {
+      id: '2',
+      employeeName: "Daniel Evangelista",
+      role: "Diretor de inovação",
+      avatar: daniel,
+      teleNumbers: [
+         '31|9.0000-0000',
+         '31|0101-0101'
+      ],
+      email: 'daniel.evangelista@mundiale.com.br',
+      qrCode: require('../public/images/qrCode.png')
+   }
+]
--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Slider from 'react-slick'
 
-import Layout from './layout/Layout.jsx';
+import EmployeesList from './EmployeesList.jsx';
+import { data } from '../data/data';
 
 import "./app.css";
 
+const settings = {
+   dots: true,
+   infinite: true,
+   arrows: false,
+   speed: 500,
+   slidesToShow: 1,
+   slidesToScroll: 1,
+}
+
 const app = (
-   <Layout>
-      <div className="list_container">
-         <ul className="employees_list">
-            <li>
-               <div className="content_list">
-                  oi
-               </div>
-            </li>
-            <li>
-               <div className="content_list">
-                  oi
-               </div>
-            </li>
-            <li>
-               <div className="content_list">
-                  oi
-               </div>
-            </li>
-            <li>
-               <div className="content_list">
-                  oi
-               </div>
-            </li>
-         </ul>
-      </div>
-   </Layout>
+   <div>
+      <EmployeesList employeeData={data[0]} />
+      <EmployeesList employeeData={data[1]} />
+   </div>
+
 )
 
 ReactDOM.render(app, document.getElementById("root"))
